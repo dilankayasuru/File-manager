8063ba432a0e4ee03c8033bdbb9de6d5cd654ed8
dilankaysuru <dilankaysuru001@gmail.com>
2023-10-25 20:07:41 +0530

    Rewrite using pathlib

--- main.py
+++ main.py
@@ -1,55 +1,96 @@
-import os
+from pathlib import Path
+from datetime import datetime
 
-# ENter your folder path in root_path
-root_path = "C:\\Users\\Name\\Desktop\\Projects\\Documents"
 
-file_paths = []
+def getDirectory():
 
-for root, directories, files in os.walk(root_path):
-    for filename in files:
-        file_path = os.path.join(root, filename)
-        file_paths.append(file_path)
+    global root_path
 
-for files in file_paths:
-    if os.path.splitext(files)[1][1:] == 'pptx':
-        powerpointDir = os.path.join(root_path, "PowerPoint")
-        try:
-            os.makedirs(powerpointDir)
-        except FileExistsError:
-            pass
-        newPath = os.path.join(powerpointDir, os.path.basename(files))
-        os.replace(files, newPath)
-    elif os.path.splitext(files)[1][1:] == 'jpeg' or os.path.splitext(files)[1][1:] == 'jpg':
-        imageDir = os.path.join(root_path, "Images")
-        try:
-            os.makedirs(imageDir)
-        except FileExistsError:
-            pass  
-        newPath = os.path.join(imageDir, os.path.basename(files))
-        os.replace(files, newPath)
-    elif os.path.splitext(files)[1][1:] == 'pdf':
-        pdfDir = os.path.join(root_path, "PDF Docs")
-        try:
-            os.makedirs(pdfDir)
-        except FileExistsError:
-            pass
-        newPath = os.path.join(pdfDir, os.path.basename(files))
-        os.replace(files, newPath)
-    elif os.path.splitext(files)[1][1:] == 'txt':
-        txtDir = os.path.join(root_path, "Text Docs")
-        try:
-            os.makedirs(txtDir)
-        except FileExistsError:
-            pass
-        newPath = os.path.join(txtDir, os.path.basename(files))
-        os.replace(files, newPath)
+    while True:
+        
+        root_path = Path(input("Enter Directory Path: ").replace('"', ""))
+
+        if root_path.is_dir():
+            print(f"\n{root_path.name} Direcotry is Identified...\n")
+            break
+        else:
+            print("\nInvalid Directory! Please try again...\n")
+            continue
+
+
+def updatePathList(pathList):
+    for filepath in root_path.rglob("*"):
+        if filepath.is_file():
+            pathList.append(filepath)
+
+
+def directoryManager(filePath, dirName, logfilePath):
+
+    with logfilePath.open(mode="a") as logfile:
+        logfile.write(f"\nOld File Location: \n{filePath}")
+
+    parentname = filePath.parent
+    fileName = filePath.name
+    if parentname.name != dirName:
+        newPath = Path(parentname, dirName, fileName)
+        if not Path(parentname / dirName).exists():
+            Path.mkdir(parentname / dirName)
+            filePath.replace(newPath)
+            with logfilePath.open(mode="a") as logfile:
+                logfile.write(f"\nNew {dirName} Directory created...\nFile Name: {fileName}\nNew file Location: \n{newPath}\n")
+        else:
+            filePath.replace(newPath)
+            with logfilePath.open(mode="a") as logfile:
+                logfile.write(f"\n{fileName} File Moved...\nNew file Location: \n{newPath}\n")
     else:
-        otherDir = os.path.join(root_path, "Other")
-        try:
-            os.makedirs(otherDir)
-        except FileExistsError:
-            pass
-        newPath = os.path.join(otherDir, os.path.basename(files))
-        os.replace(files, newPath)
+        with logfilePath.open(mode="a") as logfile:
+            logfile.write(f"\nFile did not move to a new directory\n")
+    
 
-print("Files mannaged!!!")
+def main(): 
+
+    pathList = []
+
+    getDirectory()
+
+    updatePathList(pathList)
+
+    logfilePath = Path(root_path, "Log File.txt")
+    
+    time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+    with logfilePath.open(mode="w") as logfile:
+        logfile.write(f"Time: {time} \nDirectory Location: {root_path.name}\n")
+ 
+    for filePath in pathList:
+        
+        fileExt = filePath.suffix[1:]
+
+        if fileExt == "docx":
+            dirName = "Word Documents"
+        elif fileExt == "pdf":
+            dirName = "PDF Documents"
+        elif fileExt == "txt":
+            dirName = "Text Documents"
+        elif fileExt == "jpg" or fileExt == "png" or fileExt == "webp" or fileExt == "jpeg":
+            dirName = "Image Files"
+        elif fileExt == "pptx":
+            dirName = "Powerpoint Files"
+        elif fileExt == "xlsx" or fileExt == "xls":
+            dirName = "Excel Documents"
+        elif fileExt == "mp4" or fileExt == "mkv" or fileExt == "webm" or fileExt == "avi" or fileExt == "m4v" or fileExt == "m4p":
+            dirName = "Video Files"
+        elif fileExt == "mp3":
+            dirName = "Audio Files"
+        elif fileExt == "zip" or fileExt == "rar":
+            dirName = "Zip Files"
+        elif fileExt == "exe":
+            dirName = "Executable Package"
+        else:
+            dirName = "Other Files"
+        
+        directoryManager(filePath, dirName, logfilePath)
+
+        print("Files organized...")
+
+main()
